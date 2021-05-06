32542bd91967f3ae27fd9c7b3f01c263af6516fc
Elliot Robertson <elliotrobertson53@gmail.com>
2021-05-06 09:16:38 +0100

    completed tests

--- src/booleans.js
+++ src/booleans.js
@@ -1,61 +1,94 @@
 const negate = a => {
   // your code here
+    a = !a;
+    return a
 };
 
 const both = (a, b) => {
   // your code here
+return (a && b)
 };
 
 const either = (a, b) => {
   // your code here
+  return (a || b)
 };
 
 const none = (a, b) => {
   // your code here
+    return (!a && !b)
+
 };
 
 const one = (a, b) => {
   // your code here
+  return (a == !b)
 };
 
 const truthiness = a => {
   // your code here
+    return !!a
+
 };
 
-const isEqual = (a, b) => {
+function isEqual(a, b) {
   // your code here
-};
+  return (a === b);
+}
 
 const isGreaterThan = (a, b) => {
   // your code here
+return (a > b)
+
 };
 
 const isLessThanOrEqualTo = (a, b) => {
   // your code here
+
+  return (a <= b)
 };
 
 const isOdd = a => {
   // your code here
+  return a % 2 === 1
 };
 
 const isEven = a => {
   // your code here
+  return a % 2 === 0
 };
 
 const isSquare = a => {
   // your code here
+
+  let sqrt = Math.sqrt(a);
+    return a == sqrt*sqrt;
 };
 
 const startsWith = (char, string) => {
   // your code here
+  return string.startsWith(char)
+
 };
 
 const containsVowels = string => {
-  // your code here
-};
+  // your code here;
+  const vowels = string.toUpperCase();
 
-const isLowerCase = string => {
+  return vowels.includes("A") || 
+  vowels.includes("E") ||vowels.includes("I") || 
+  vowels.includes("O") || vowels.includes("U");
+
+};
+  
+
+
+function isLowerCase(string) {
   // your code here
+
+  return string === string.toLowerCase();
+
+  
 };
 
 module.exports = {
--- src/objects.js
+++ src/objects.js
@@ -1,41 +1,56 @@
 const createPerson = (name, age) => {
   // your code here
-};
+  return  {name:name, age: age}
 
+};
 const getName = object => {
   // your code here
+    return object.name
 };
 
 const getProperty = (property, object) => {
   // your code here
+    return object.[property]
 };
 
 const hasProperty = (property, object) => {
   // your code here
+    return object.hasOwnProperty(property)
+    
 };
 
 const isOver65 = person => {
   // your code here
+  return person.age > 65
 };
 
 const getAges = people => {
   // your code here
+  return people.map((person) => person.age)
 };
 
 const findByName = (name, people) => {
   // your code here
+  return people.find(x => x.name === name )
 };
 
 const findHondas = cars => {
   // your code here
+  return cars.filter(car => car.manufacturer === "Honda")
 };
 
 const averageAge = people => {
   // your code here
-};
+  return people.reduce((prev, person) => prev + person.age, 0) / people.length;
+}
 
 const createTalkingPerson = (name, age) => {
   // your code here
+  return {name: name, age: age,
+  introduce: function(othername){
+    return "Hi " +othername+ ", my name is " +name+ " and I am " + age +"!"
+  }
+}
 };
 
 module.exports = {
